437943c16aa1f4f696260ade043c5257abace985
Jordan <climbjn189@gmail.com>
2020-07-16 02:16:40 -0400

    Updated ETL and commented

--- indeedETL.py
+++ indeedETL.py
@@ -1,31 +1,60 @@
 import sys
 from bs4 import BeautifulSoup
+from datetime import date
 import json
 
 if __name__ == "__main__":
+    #Open and read in the actual data scraped
     fin = open(sys.argv[1], "r")
     data = fin.read().split("|")
+    fin.close()
+
+    #Open and read in the tags the user has added
+    fin = open(sys.argv[2], "r")
+    goodTags = fin.read().split(",")
     fin.close()
 
     results = []
 
     for item in data:
+        #Make sure the item actually has data
         if len(item) > 1:
             soup = BeautifulSoup(item, 'html.parser')
             temp = {}
+            #Grab the fields from the data
             temp["jobTitle"] = soup.find("div", id="vjs-jobtitle").string
             temp["companyName"] = soup.find("span", id="vjs-cn").string
-            temp["location"] = soup.find("span", id="vjs-loc").getText().strip()[2:]
+            #For the location split it into state, city, and zip if a zip exists
+            location = {}
+            loc = soup.find("span", id="vjs-loc").getText().strip()[2:]
+            location["city"], locState = loc.split(",")
+            if len(locState) > 3:
+                location["zip"] = locState[4:9]
+                location["state"] = locState[1:3]
+            else:
+                location["state"] = locState[1:3]
+            temp["location"] = location
             temp["desc"] = soup.find("div", id="vjs-desc").getText()
+            #If there is a dollar sign then there is a salary field so grab that
             index = item.find("$")
             if index != -1:
                 temp["salary"] = item[index:item.find("<", index)]
+            #If there is a button that has that class then there is an apply button with a link to get
             applyButton = soup.find("a", class_="view-apply-button blue-button")
             if applyButton != None:
                 temp["applyLink"] = "indeed.com" + applyButton.get("href")
-            #TODO: THIS
-            #temp["url"] = soup.find("p", class_="URL").getText()
-            title = ",".join([temp["jobTitle"], temp["companyName"], temp["location"]])
+            #The URL of the page was added on by the scraper
+            temp["url"] = soup.find("p", class_="URL").getText()
+            itemTag = []
+            tempDesc = temp["desc"].lower()
+            #Loop through every tag and add it if it's found within the description
+            for goodTag in goodTags:
+                if tempDesc.find(goodTag) >= 0:
+                    itemTag.append(goodTag)
+            temp["tags"] = itemTag
+            temp["date"] = date.today().__str__()
+            #The title is the hopefully unique key for every posting made up of the title, company, and location
+            title = (",".join([temp["jobTitle"], temp["companyName"], loc])).replace(" ", "")
             results.append({title: temp})
 
     print(json.dumps(results, sort_keys=True, indent=4))
--- indeedScraper.py
+++ indeedScraper.py
@@ -6,87 +6,112 @@ import random
 import time
 import sys
 
-class scraper:
+#Class that handles all the scraping for Indeed
+class IndeedScraper:
     #age = "30"
     age = "7"
     sort = "date"
     jobType = "fulltime"
     expLevel = ["entry_level", "mid_level"]
 
+    #Sets the job and location for the scrape
     def __init__(self, job, location):
         self.job = job
         self.location = location
 
+    #Gets the exact URL to go to
     def getURL(self):
         baseURL = "https://www.indeed.com/jobs?"
         jobURL = "q=" + self.job.replace(" ", "+")
         locationURL = "l=" + self.location.replace(" ", "+")
         jobTypeURL = "jt=" + self.jobType
+        #Currently on does entry level experience level
         expLevelURL = "explvl=" + self.expLevel[0]
         dateURL = "fromage=" + self.age
         sortURL = "sort=" + self.sort
         fullURL = baseURL+jobURL+"&"+locationURL+"&"+jobTypeURL+"&"+expLevelURL+"&"+dateURL+"&"+sortURL
         return fullURL
 
+    #Does all the actual scraping here
     def scrape(self):
         URL = self.getURL()
+        #Set the filename to what we scraped
         fileName = self.job + "_" + self.location + "_" + self.jobType + "_" + self.expLevel[0] + ".txt"
         fout = open(fileName.replace(" ", "-"), "a")
 
+        #In case of exceptions this is all in a try catch finally
         try:
+            #Open the driver, set it to wait 1 second, and open the url
             driver = webdriver.Firefox()
             driver.implicitly_wait(1)
             driver.get(URL)
+            #If for some reason we haven't loaded Indeed, raise an error
             if "Indeed" not in driver.title:
                 raise Exception("Navigated off Indeed somehow")
-            time.sleep(2)
+            #Let stuff load in
+            time.sleep(1)
 
             moreToScrape = True
+            #Keep scraping until we hit the last page and there isn't anything left
             while moreToScrape:
 
+                #Every job is represented by a jobsearch-SerpJobCard
                 jobs = driver.find_elements_by_class_name('jobsearch-SerpJobCard')
                 for job in jobs:
+                    #Have to keep scrolling the items into view before we click on them or it errors
                     driver.execute_script("arguments[0].scrollIntoView();", job)
                     job.click()
+                    #Again if we somehow got off Indeed by clicking a link then raise an error
                     if "Indeed" not in driver.title:
                         raise Exception("Navigated off Indeed")
-                    #TODO: See how this replace works
+                    #The info we want is the innerHTML of the vjs-container. Replace the | with / to prevent run-ins with our ETL
                     fout.write(driver.find_element_by_id('vjs-container').get_attribute('innerHTML').replace("|", "/"))
-                    #TODO: Make sure URL is there
+                    #Add a small field on the end with the URL for use in the ETL
                     fout.write("<p class=\"URL\">" + driver.current_url + "</p>")
+                    #Add a pipe to seperate entries
                     fout.write("|")
+                    #Sleep for a random short amount of time to hopefully fool Indeed
                     time.sleep(random.random())
             
+                #Look for an item with the css on it that denotes it is the next page arrow
+                #If its there then click it, otherwise we are on the last page and thus done with scraping
                 nextPage = driver.find_elements_by_css_selector("a[aria-label='Next']")
                 if len(nextPage) == 1:
+                    #Click on the next page, wait a sec to load in
                     nextPage[0].click()
                     time.sleep(1)
+                    #If a popup x appears then a popup has appeared and we need to close it so just click it
                     popupX = driver.find_elements_by_class_name('popover-x-button-close')
                     if len(popupX) == 1:
                         popupX[0].click()
                         time.sleep(random.random())
-                    else:
+                    #If there are too many popup X buttons panic and throw an error. What has the world come too...
+                    elif len(popupX) > 1:
                         raise Exception("Too many X buttons")
                 else:
                     moreToScrape = False
 
+        #Print any exception we have
         except Exception as e:
             print(e)
 
+        #Finally quit the driver and close the open file
         finally:
             driver.quit()
             fout.close()
 
 
 if __name__ == "__main__":
+    #Grab the configurations from file
     with open(sys.argv[1], "r") as configFile:
         config = configFile.read()
     config = config.split("\n")
     jobs = config[0].split(",")[1:]
     locations = config[1].split(",")[1:]
 
+    #TODO: Uncomment this when ready
     #for job in jobs:
     #    for location in locations:
     #        scraper(job, location).scrape()
     #        time.sleep(5 + random.random())
-    scraper(jobs[0], locations[0]).scrape()
+    IndeedScraper(jobs[0], locations[0]).scrape()
